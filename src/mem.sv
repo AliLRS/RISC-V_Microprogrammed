a8ac53d40d8505e047626182bd30d76fadab1578
AliLRS <sorush.lamei@gmail.com>
2023-06-26 00:13:44 +0330

    add memory

---
+++ src/mem.sv
@@ -0,0 +1,19 @@
+module mem(
+        input  logic        clk, 
+        input  logic        we,
+        input  logic [31:0] a, wd,
+        output logic [31:0] rd);
+
+    logic [31:0] RAM[63:0];
+
+    initial
+        $readmemh("riscvtest.txt",RAM);
+
+    assign rd = RAM[a[31:2]]; // word aligned
+
+    always_ff @(posedge clk) begin
+        if (we) 
+            RAM[a[31:2]] <= wd;
+    end
+
+endmodule
